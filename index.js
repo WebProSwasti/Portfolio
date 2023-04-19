c94f85cd2facc057c5de4fbc0b058bf25b666626
WebProSwasti <131282434+WebProSwasti@users.noreply.github.com>
2023-04-19 21:36:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple", {
+    strings: ["Frontend Developer", "YouTuber", "Blogger", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
